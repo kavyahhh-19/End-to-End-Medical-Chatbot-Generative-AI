31a7d17f2535f6c5e28aa97eb360d8fbc6641fb1
kavyahhh-19 <vempatik6@gmail.com>
2025-07-07 14:30:06 +0530

    updated

--- app.py
+++ app.py
@@ -1,45 +1,40 @@
-
-
-
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, request
 from src.helper import download_hugging_face_embeddings
 from langchain_pinecone import PineconeVectorStore
-from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOllama
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 from src.prompt import *
 import os
+
 print("🚀 Current working directory:", os.getcwd())
 print("📂 Files & folders here:", os.listdir())
 print("✅ Does 'templates/index.html' exist?:", os.path.exists('templates/index.html'))
 
-
 app = Flask(__name__, template_folder='templates')
-
 
 load_dotenv()
 
-PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
-OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+
 
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
 
 embeddings = download_hugging_face_embeddings()
 
-index_name = "medicalbot" 
-# Embed each chunk and upsert the embeddings into your Pinecone index.
+index_name = "medicalbot"
+
 docsearch = PineconeVectorStore.from_existing_index(
     index_name=index_name,
     embedding=embeddings
 )
 
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k": 3})
 
-retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
-
-chatModel = ChatOpenAI(model="gpt-3.5-turbo")
+chatModel = ChatOllama(model="llama3")
 
 prompt = ChatPromptTemplate.from_messages(
     [
@@ -51,13 +46,9 @@ prompt = ChatPromptTemplate.from_messages(
 question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
 rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
-
 @app.route("/")
 def index():
     return render_template('index.html')
-
-
-
 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
@@ -67,12 +58,8 @@ def chat():
         answer = response["answer"]
     except Exception as e:
         print("Error:", e)
-        answer = "⚠ Sorry, the bot can't answer now (maybe out of quota)."
+        answer = "⚠ Sorry, the bot can't answer now."
     return str(answer)
 
-
-
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
-
+    app.run(host="0.0.0.0", port=8080, debug=True)
